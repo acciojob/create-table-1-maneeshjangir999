d61493052a369cf9f09b4d40e49497b322da9f06
Maneesh Jangir <122728056+maneeshjangir999@users.noreply.github.com>
2025-02-27 10:43:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let table = document.getElementById("sampleTable")
 function insert_Row() {
-    //Write your code here
-  
-  
+	let newRow = table.insertRow(0)
+	let leftCell = newRow.insertCell(0)
+	let rightCell = newRow.insertCell(1)
+	leftCell.textContent = "New Cell1"
+	rightCell.textContent = "New Cell2"
+	
+	
 }
